f78083a7ee6b7b1cad073204925eb8cd8785b92c
Le Trung Son <sonmartin2003@gmail.com>
2024-03-31 17:43:33 +0700

    debugged

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,21 +14,19 @@ pipeline {
         dockerTool 'docker'
     }
     stages {
-        // stage("build and test") {
-        //     steps {
-        //         // sh 'npm install'    // ví dụ: cài đặt các phụ thuộc npm
-        //         // sh 'node app.js'
-        //         // sh 'npm test'     // ví dụ: chạy các bài kiểm thử
-        //     }
-        // }
-        // stage("Docker login and push docker image") {
-        //     steps {
-        //         withBuildConfiguration {
-        //             sh "docker build -t sonmartin/devops-todo-apps:0.0.2 ."
-        //             sh "docker push sonmartin/devops-todo-apps:0.0.2"
-        //         }
-        //     }
-        // }
+        stage("build and test") {
+            steps {
+                sh "docker build -t sonmartin/devops-todo-apps:0.0.2 ."
+            }
+        }
+        stage("Docker login and push docker image") {
+            steps {
+                withBuildConfiguration {
+                    sh 'docker login -u "$repository_username" -p "$repository_password"'
+                    sh "docker push sonmartin/devops-todo-apps:0.0.2"
+                }
+            }
+        }
         stage("deploy") {
             steps {
                 // withBuildConfiguration {
@@ -43,9 +41,13 @@ pipeline {
                 sshagent(credentials: [SSH_ID_REF]) {
                         // sh "ssh -tt -vvv root@ec2-18-141-234-249.ap-southeast-1.compute.amazonaws.com"
                         // sh "ssh -tt -vvv root@ec2-18-142-231-213.ap-southeast-1.compute.amazonaws.com"
-                        sh "ssh -tt -vvv ec2-54-252-190-112.ap-southeast-2.compute.amazonaws.com"
-                        sh "docker pull sonmartin/devops-todo-apps:0.0.1"
-                        sh "docker run -p 8000:8000 sonmartin/devops-todo-apps:0.0.1"
+                        // sh "ssh -tt -vvv ec2-54-252-190-112.ap-southeast-2.compute.amazonaws.com"
+                        // sh "docker pull sonmartin/devops-todo-apps:0.0.1"
+                        // sh "docker run -p 8000:8000 sonmartin/devops-todo-apps:0.0.1"
+                        sh '''
+                        docker run -d --rm --name todo-app -p 8000:8000 sonmartin/devops-todo-apps:0.0.2
+                        docker ps
+                        '''
                     }
             }
         }
